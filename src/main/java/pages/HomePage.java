dd95805442a1da9858037477dd4f118d8556e03b
sorinavancea <sorina.sorina61@yahoo.com>
2021-06-28 16:09:58 +0300

    HOME PAGE - added all the tests

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,7 +1,6 @@
 package pages;
 
 import java.util.ArrayList;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,23 +8,23 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 	protected WebDriver driver;
-	
+
 	public HomePage(WebDriver driver){
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	//WebElements//
-	
+
 	@FindBy(xpath = "//div[@id='i6ksxrtk']/h1/a")
 	private WebElement header;
-	
+
 	@FindBy(xpath = "//p[.='HOME']")
 	private WebElement homeTab;
-	
+
 	@FindBy(xpath = "//div/wix-image/img")
 	private WebElement background;
-	
+
 	@FindBy(xpath = "//*[@id='img_0_i6rlbitx']/img")
 	private WebElement facebookIcon;
 
@@ -43,19 +42,166 @@ public class HomePage {
 
 	@FindBy(xpath = "//*[@id='i3175p-i6rlbitx']/a")
 	private WebElement pinterestClick;
-	
+
 	@FindBy(xpath = "//iframe[@title='Wix Hotels']")
 	private WebElement wixHotels;
-	
+
+	@FindBy(xpath = "//*[@id=\"adults-label\"]")
+	private WebElement adultsLabel;
+
+	@FindBy(xpath = "//*[@id=\"adults\"]/a[1]")
+	private WebElement upAdultsClick;
+
+	@FindBy(xpath = "//*[@id=\"adults\"]/span[contains(text(),'3')]")
+	private WebElement twoUpAdults;
+
+	@FindBy(xpath = "//*[@id=\"adults\"]/a[2]")
+	private WebElement downAdults;
+
+	@FindBy(xpath = "//*[@id='adults']/span[contains(text(),'2')]")
+	private WebElement oneDownAdults;
+
+	@FindBy(xpath = "//*[@id=\"i6tj0u8x\"]/a/span")
+	private WebElement bookNowClick;
+
+	@FindBy(xpath = "/html/body/div/div/div[4]/div/header/div/div[2]/div[2]/div/div/div[1]/a[@href='https://ancabota09.wixsite.com/intern/rooms']")
+	private WebElement roomsDisplay;
+
+	@FindBy(xpath = "//*[@id=\"children-label\"]")
+	private WebElement kidsLabel;
+
+	@FindBy(xpath = "//*[@id=\"children\"]/a[1]")
+	private WebElement upKidsClick;
+
+	@FindBy(xpath = "//*[@id=\"children\"]/span[contains(text(),'2')]")
+	private WebElement twoUpKids;
+
+	@FindBy(xpath = "//*[@id=\"children\"]/a[2]")
+	private WebElement downKids;
+
+	@FindBy(xpath = "//*[@id=\"children\"]/span[contains(text(),'1')]")
+	private WebElement oneDownKids;
+
+	@FindBy(className  = "color_15")
+	private WebElement isaddress;
+
+	@FindBy(xpath = "//*[@id='i71wvfxg']/p[1]")
+	private WebElement address1;
+
+	@FindBy(xpath = "//*[@id='i71wvfxg']/p[2]")
+	private WebElement address2;
+
+	@FindBy(xpath  = "//p[@class='font_8']")
+	private WebElement iscontact;
+
+	@FindBy(xpath = "//*[@id='i71ww6nk']/p[1]/object/a")
+	private WebElement contact1;
+
+	@FindBy(xpath = "//*[@id='i71ww6nk']/p[1]/object/a")
+	private WebElement accept;
+
+	@FindBy(xpath = "//*[@id='i71ww6nk']/p[2]")
+	private WebElement contact2;
+
+	@FindBy(xpath = "//*[@id='i6ktzy38']/p/span")
+	private WebElement home;
+
+	@FindBy(xpath = "//*[@id='i71wwqnj']/p[1]")
+	private WebElement home1;
+
+	@FindBy(xpath = "//*[@id='i71wwqnj']/p[2]")
+	private WebElement home2;
+
+	@FindBy(xpath = "//span[@class='nr31w']" )
+	private WebElement bookNow;
+
+	@FindBy(xpath = "//iframe[@title='Wix Chat']")
+	private WebElement wixChat;
+
+	@FindBy(xpath = "//*[@id=\"minimized-chat\"]")
+	private WebElement minimizeChat;
+
+	@FindBy(xpath = "//form[@class='MvNna']")
+	private WebElement find;
+
+	@FindBy(xpath = "//div[@class='wqK4U _1aQMT']")
+	private WebElement expectedEmoji;
+
+	@FindBy(xpath = "//button[@class='_14Qkd']")
+	private WebElement emojiBtn;
+
+	@FindBy(xpath = "//*[@id=\"emojis-tab-content-0\"]/button[1]")
+	private WebElement selectEmoji;
+
+	@FindBy(xpath = "//button[@class='_1TZEF Bgzzt']")
+	private WebElement chatButton;
+
+	@FindBy(xpath = "//*[@id=\"name\"]")
+	private WebElement name;
+
+	@FindBy(xpath = "//*[@id=\"email\"]")
+	private WebElement email;
+
+	@FindBy(xpath = "//*[@id=\"message\"]")
+	private WebElement message;
+
+	@FindBy(xpath = "//button[@class='_1nJDH']")
+	private WebElement submit;
+
+	@FindBy(xpath = "//div[contains(text(),'Thanks! Message us here.')]")
+	private WebElement submitExpected;
+
+	@FindBy(xpath = "//button[@class='_14Qkd MXzRy']")
+	private WebElement attach;
+
+	@FindBy(xpath = ".//input[@type='file']")
+	private WebElement file;
+
+	@FindBy(xpath = "//button[@class='_14Qkd _3JsPd']")
+	private WebElement sendBtn;
+
+	@FindBy(xpath = "//button[@id='check-in'][1]")
+	private WebElement dataBtnCheckIn;
+
+	@FindBy(xpath = "//iframe[contains(@name,'i6kppi75')]")
+	private WebElement frame1;
+
+	@FindBy(xpath = "//*[@id='check-in-value']")
+	private WebElement actualDateCheckIn;
+
+	@FindBy(xpath = "//span[contains(text(),'30')]")
+	private WebElement dataCheckIn;
+
+	@FindBy(xpath = "//iframe[@title='Wix Hotels']")
+	private WebElement wixxIframe;
+
+	@FindBy(xpath = "//button[@day-button-aria='day']/span[contains(text(),'6')]")
+	private WebElement checkOutData;
+
+	@FindBy(xpath = "//*[@id='check-out-value']")
+	private WebElement actualDateCheckOut;
+
+	@FindBy(xpath = "//button[@wix-bi='SEARCH']")
+	private WebElement searchButton;
+
+	@FindBy(xpath ="//*[@id='search-widget']/form/ul/li[6]/button/span/span")
+	private WebElement csearchButton;
+
+	@FindBy(xpath ="//button[@day-button-aria='day']/span[contains(text(),'30')]")
+	private WebElement dataCheckIn2;
+
+	@FindBy(xpath ="/html/body/div/main/div/nav/button[2]")
+	private WebElement nextMonth;
+
 	//Actions on WebElements//
 	public String getPageTitle(){
 		return driver.getTitle();
 	}
-	
+
 	public String getHeaderText(){
 		return header.getText();
 	}
-		
+
 	public boolean homeTabDisplayed(){
 		return homeTab.isDisplayed();
 	}
@@ -63,7 +209,7 @@ public class HomePage {
 	public boolean backgroundDisplayed() {
 		return background.isDisplayed();
 	}
-	
+
 	public boolean facebookIconDisplayed() {
 		return facebookIcon.isDisplayed();
 	}
@@ -107,4 +253,235 @@ public class HomePage {
 		}
 	}
 
+	public WebDriver frameWixHotels() {
+		return driver.switchTo().frame(wixHotels);
+	}
+
+	public WebDriver frameWixHotels1() {
+		return driver.switchTo().frame(wixHotels);
+	}
+
+	public boolean adultsDisplayed() {
+		return adultsLabel.isDisplayed();
+	}
+
+	public void adultsUpClicked() {
+		upAdultsClick.click();
+	}
+
+	public boolean twoUpAdultsDisplayed() {
+		return twoUpAdults.isDisplayed();
+	}
+
+	public void adultsDownClicked() {
+		downAdults.click();
+	}
+
+	public boolean oneDownAdultsDisplayed() {
+		return oneDownAdults.isDisplayed();
+	}
+
+	public boolean kidsDisplayed() {
+		return kidsLabel.isDisplayed();
+	}
+
+	public void kidsUpClicked() {
+		upKidsClick.click();
+	}
+
+	public boolean twoUpKidsDisplayed() {
+		return twoUpKids.isDisplayed();
+	}
+
+	public void kidsDownClicked() {
+		downKids.click();
+	}
+
+	public boolean oneDownKidsDisplayed() {
+		return oneDownKids.isDisplayed();
+	}
+
+	public boolean addressDisplayed() {
+		return isaddress.isDisplayed();
+	}
+
+	public boolean addressDisplayed1() {
+		return address1.isDisplayed();
+	}
+
+	public boolean addressDisplayed2() {
+		return address2.isDisplayed();
+	}
+
+	public boolean contactDisplayed() {
+		return iscontact.isDisplayed();
+	}
+
+	public boolean contactDisplayed1() {
+		return contact1.isDisplayed();
+	}
+
+	public boolean contactDisplayed2() {
+		return contact2.isDisplayed();
+	}
+
+	public boolean contactAcceptDisplayed2() {
+		return accept.isDisplayed();
+	}
+
+	public boolean homeDisplayed1() {
+		return home.isDisplayed();
+	}
+
+	public boolean homeDisplayed2() {
+		return home1.isDisplayed();
+	}
+
+	public boolean homeDisplayed3() {
+		return home2.isDisplayed();
+	}
+
+	public boolean bookNowDisplayed() {
+		return bookNow.isDisplayed();
+	}
+
+	public boolean bookNowClicked() {
+		return bookNowClick.isDisplayed();
+	}
+
+	public boolean roomsDisplayed() {
+		return roomsDisplay.isDisplayed();
+	}
+
+	public void switchToChat() {
+		driver.switchTo().frame(wixChat);
+	}
+
+	public boolean chatBtnisDisplayed() {
+		return minimizeChat.isDisplayed();
+	}
+
+	public void chatBtnClicked() {
+		chatButton.click();
+	}
+
+	public void emojiBtnClicked() {
+		emojiBtn.click();
+	}
+
+	public boolean emojiBtnDisplayed() {
+		return emojiBtn.isDisplayed();
+	}
+
+	public void emojiClicked() {
+		selectEmoji.click();
+	}
+
+	public boolean emojiDisplayed() {
+		return selectEmoji.isDisplayed();
+	}
+
+	public void sendEmojiClicked() {
+		sendBtn.click();
+	}
+
+	public boolean expectedEmojiDisplayed() {
+		return expectedEmoji.isDisplayed();
+	}
+
+	public boolean formDisplayed() {
+		return find.isDisplayed();
+	}
+
+	public void nameClicked() {
+		name.click();
+	}
+
+	public boolean nameDisplayed() {
+		return name.isDisplayed();
+	}
+
+	public void nameSendDisplayed() {
+		name.sendKeys("Sorina Vancea");
+	}
+
+	public void emailClicked() {
+		email.click();
+	}
+
+	public boolean emailDisplayed() {
+		return email.isDisplayed();
+	}
+
+	public void emailSendDisplayed() {
+		email.sendKeys("sorina.sorina61@yahoo.com");
+	}
+
+	public void messageClicked() {
+		message.click();
+	}
+
+	public boolean messageDisplayed() {
+		return email.isDisplayed();
+	}
+
+	public void messageSendDisplayed() {
+		message.sendKeys("Buna ziua! Acesta este mesajul meu!");
+	}
+
+	public void submitClicked() {
+		submit.click();
+	}
+
+	public boolean submitDisplayed() {
+		return submitExpected.isDisplayed();
+	}
+
+	public boolean attachmentDisplayed() {
+		return attach.isDisplayed();
+	}
+
+	public void addFileDisplayed() {
+		file.sendKeys("C:\\Users\\z004c2sx\\Documents\\TestCase - Copy.xlsx");
+	}
+
+	public void checkInClicked() {
+		dataBtnCheckIn.click();
+	}
+
+	public void switchToframe() {
+		driver.switchTo().frame(frame1);
+	}
+
+	public void dataCheckInClicked() {
+		dataCheckIn.click();
+	}
+
+	public String actualDateCheckIn() {
+		return actualDateCheckIn.getText();
+	}
+
+	public void dataCheckOutClicked() {
+		checkOutData.click();
+	}
+
+	public String actualDateCheckOut() {
+		return actualDateCheckOut.getText();
+	}
+
+	public boolean searchBtnDisplayed() {
+		return searchButton.isDisplayed();
+	}
+
+	public void searchBtnClicked() {
+		csearchButton.click();
+	}
+
+	public void dataCheckIn1Clicked() {
+		dataCheckIn2.click();
+	}
+
+	public void nextMonthClicked() {
+		nextMonth.click();
+	}
 }
--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -2,8 +2,12 @@ package tests;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -19,56 +23,356 @@ public class HomeTest {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\z004c2sx\\Downloads\\chromedriver.exe");
 		driver = new ChromeDriver(); 
 		driver.manage().window().maximize();
-		
+
 		homePage = new HomePage(driver);
-		
+
 		driver.get("https://ancabota09.wixsite.com/intern"); 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 	}
 
-	    @AfterTest
-	    public void afterTest() {
-	    	if (driver != null)
-				driver.quit();
-	    }
-	    
-	    
-	    @Test
-	    public void verifyHomeTab() {
-	    	 //verify page title
-	    	 Assert.assertEquals(homePage.getPageTitle(), "HOME | Intern", "Title is not as expected");
-	    	 Assert.assertEquals(homePage.getHeaderText(), "HOME & AWAY", "Not as expected");
-	      	 Assert.assertTrue(homePage.homeTabDisplayed(), "Home tab is not displayed"); 
-	    }
-	    
-	    @Test
-		public void verifyBackground() { 
-			Assert.assertTrue(homePage.backgroundDisplayed(), "Background is not displayed");
-		}
-	    
-	    @Test 
-		public void verifySocialMedia() throws InterruptedException { 
-			Assert.assertTrue(homePage.facebookIconDisplayed(), "Facebook icon is not displayed");
-			Assert.assertTrue(homePage.twiterIconDisplayed(), "Twiter icon is not displayed");
-			Assert.assertTrue(homePage.pinterestIconDisplayed(), "Pinterest icon is not displayed");
-			homePage.facebookClicked();
-			homePage.switchTo(1);
-		    Assert.assertTrue(homePage.getUrl().contains("facebook"), "Facebook page is not displayed");
-		    homePage.closeDriver();
-		    homePage.switchTo(0);
-		    
-		    homePage.twitterClicked();
-		    homePage.switchTo(1);
-			Assert.assertTrue(homePage.getUrl().contains("twitter"), "Twiter page is not displayed");
-			homePage.closeDriver();
-			homePage.switchTo(0);
-			
-			//Click the Pinterest icon
-			homePage.pinterestClicked();
-			homePage.switchTo(1);
-			Assert.assertTrue(homePage.getUrl().contains("pinterest"), "Pinterest page is not displayed");
-			homePage.closeDriver();
-		
-		}
+	@AfterTest
+	public void afterTest() {
+		if (driver != null)
+			driver.quit();
+	}
 
+
+	@Test
+	public void verifyHomeTab() {
+		//verify page title
+		Assert.assertEquals(homePage.getPageTitle(), "HOME | Intern", "Title is not as expected");
+		Assert.assertEquals(homePage.getHeaderText(), "HOME & AWAY", "Not as expected");
+		Assert.assertTrue(homePage.homeTabDisplayed(), "Home tab is not displayed"); 
+	}
+
+	@Test
+	public void verifyBackground() { 
+		Assert.assertTrue(homePage.backgroundDisplayed(), "Background is not displayed");
+	}
+
+	@Test 
+	public void verifySocialMedia() throws InterruptedException { 
+		Assert.assertTrue(homePage.facebookIconDisplayed(), "Facebook icon is not displayed");
+		Assert.assertTrue(homePage.twiterIconDisplayed(), "Twiter icon is not displayed");
+		Assert.assertTrue(homePage.pinterestIconDisplayed(), "Pinterest icon is not displayed");
+		homePage.facebookClicked();
+		homePage.switchTo(1);
+		Assert.assertTrue(homePage.getUrl().contains("facebook"), "Facebook page is not displayed");
+		homePage.closeDriver();
+		homePage.switchTo(0);
+
+		homePage.twitterClicked();
+		homePage.switchTo(1);
+		Assert.assertTrue(homePage.getUrl().contains("twitter"), "Twiter page is not displayed");
+		homePage.closeDriver();
+		homePage.switchTo(0);
+
+		//Click the Pinterest icon
+		homePage.pinterestClicked();
+		homePage.switchTo(1);
+		Assert.assertTrue(homePage.getUrl().contains("pinterest"), "Pinterest page is not displayed");
+		homePage.closeDriver();
+
+	}
+
+	@Test
+	public void verifyAdults() throws InterruptedException { 
+		Thread.sleep(1000);
+		homePage.frameWixHotels();
+
+		Assert.assertTrue(homePage.adultsDisplayed(), "Adults field is not displayed");
+		homePage.adultsUpClicked();
+		homePage.adultsUpClicked();
+
+		Thread.sleep(1000);
+		Assert.assertTrue( homePage.twoUpAdultsDisplayed(), "There are not 3 adults");
+		Thread.sleep(1000);
+
+		homePage.adultsDownClicked();
+
+		Assert.assertTrue( homePage.oneDownAdultsDisplayed(), "There are not 2 adults");
+		Thread.sleep(1000);
+
+	}
+
+	@Test
+	public void verifyKids() throws InterruptedException { 
+		homePage.frameWixHotels1();
+
+		Assert.assertTrue(homePage.kidsDisplayed(), "Kids field is not displayed");
+		homePage.kidsUpClicked();
+		homePage.kidsUpClicked();
+
+		Assert.assertTrue( homePage.twoUpKidsDisplayed(), "There are not 3 kids");
+		Thread.sleep(1000);
+
+		homePage.kidsDownClicked();
+
+		Assert.assertTrue( homePage.oneDownKidsDisplayed(), "There are not 2 kids");
+		Thread.sleep(1000);
+
+	}
+
+
+	@Test
+	public void verifyChat() throws InterruptedException {
+		Thread.sleep(3000);
+
+		//swich to frame
+		homePage.switchToChat();
+
+		Thread.sleep(1000);
+
+		//Validate that the Chat button exists 
+		Assert.assertTrue(homePage.chatBtnisDisplayed(), "Chat is not displayed");
+
+		//Click the chat button
+		homePage.chatBtnClicked();
+
+		Thread.sleep(1000);
+
+		//Enter a message/emoji 
+		homePage.emojiBtnClicked();
+		Assert.assertTrue(homePage.emojiBtnDisplayed(), "Emoji button is not selected");
+
+		Thread.sleep(1000);
+		homePage.emojiClicked();
+		Assert.assertTrue(homePage.emojiDisplayed(), "Emoji is not selected");
+
+		Thread.sleep(1000);
+
+		homePage.sendEmojiClicked();
+
+		Assert.assertTrue(homePage.expectedEmojiDisplayed(), "Emoji is not dispayed");
+
+		//Enter all the information and click the submit button
+		Assert.assertTrue(homePage.formDisplayed(), "Chat is not displayed");
+
+		homePage.nameClicked();
+		homePage.nameSendDisplayed();
+		Assert.assertTrue(homePage.nameDisplayed(), "Name is not displayed");
+
+		Thread.sleep(1000);
+
+		homePage.emailClicked();
+		homePage.emailSendDisplayed();
+		Assert.assertTrue(homePage.emailDisplayed(), "Email is not displayed");
+
+		Thread.sleep(1000);
+
+		homePage.messageClicked();
+		homePage.messageSendDisplayed();
+		Assert.assertTrue(homePage.messageDisplayed(), "Name is not displayed");
+
+		Thread.sleep(1000);
+
+		homePage.submitClicked();
+		Assert.assertTrue(homePage.submitDisplayed(), "Submit has a problem");
+
+		Thread.sleep(3000);
+
+		//Click the attachment button
+		Assert.assertTrue(homePage.attachmentDisplayed(), "Chat is not displayed");
+		homePage.addFileDisplayed();
+
+		Thread.sleep(5000);
+		driver.switchTo().defaultContent();
+	}
+
+	@Test 
+	public void verifyGeneralInfo() { 
+		//Validate that the address exists at the bottom of the page
+		Assert.assertTrue(homePage.addressDisplayed(), "Address is not displayed");
+		Assert.assertTrue(homePage.addressDisplayed1(), "Address is not displayed");
+		Assert.assertTrue(homePage.addressDisplayed2(), "Address is not displayed");
+
+
+		//Validate that the contact information exist at the bottom of the page
+		Assert.assertTrue(homePage.addressDisplayed(), "Contact is not displayed");
+		Assert.assertTrue(homePage.addressDisplayed1(), "Contact is not displayed");
+
+		homePage.contactAcceptDisplayed2();
+		driver.switchTo().defaultContent();
+
+		Assert.assertTrue(homePage.contactDisplayed2(), "Contact is not displayed");
+
+
+		//Validate that the site information exist at the bottom of the page
+		Assert.assertTrue(homePage.homeDisplayed1(), "Home&Away is not displayed");
+		Assert.assertTrue(homePage.homeDisplayed2(), "Home&Away is not displayed");
+		Assert.assertTrue(homePage.homeDisplayed3(), "Home&Away is not displayed");
+		driver.switchTo().defaultContent();
+	}
+
+	@Test
+	void verifyBookNow() throws InterruptedException {
+		//Validate that the Book Now button exists 
+		Assert.assertTrue(homePage.bookNowDisplayed(), "Book Now is not displayed");
+
+		//Click the Book Now button
+		homePage.bookNowClicked();
+
+		Assert.assertTrue( homePage.roomsDisplayed(), "Rooms page is not displayed");
+
+		Thread.sleep(1000);
+	}
+
+	@Test 
+	public void verifySearch() throws InterruptedException { 
+		Thread.sleep(3000);
+
+		//Validate that frame exists
+		homePage.frameWixHotels();
+		Thread.sleep(3000);
+
+		//Click the check in field
+		homePage.checkInClicked();
+		Thread.sleep(1000);
+
+		//switch to default
+		driver.switchTo().defaultContent();
+		Thread.sleep(1000);
+
+		//Click on a date (later than today)
+		//switch to check-in frame
+		homePage.switchToframe();
+		Thread.sleep(1000);
+
+		//select a date ->later than today
+		Thread.sleep(1000);
+		homePage.dataCheckInClicked();
+		Thread.sleep(3000);
+
+		homePage.frameWixHotels1();
+		Assert.assertEquals(homePage.actualDateCheckIn(), "29 Jun 2021");
+
+		//switch to default
+		driver.switchTo().defaultContent();
+
+		homePage.switchToframe();
+
+		Thread.sleep(1000);
+		homePage.dataCheckOutClicked();
+
+		Thread.sleep(1000);
+
+		homePage.frameWixHotels();
+		Assert.assertEquals(homePage.actualDateCheckOut(), "6 Jul 2021");
+
+		Thread.sleep(1000);
+		driver.switchTo().defaultContent();
+
+		Thread.sleep(1000);
+		homePage.frameWixHotels1();
+
+		Thread.sleep(1000);
+
+		//Validate that the Search button exists
+		Assert.assertTrue(homePage.searchBtnDisplayed(), "Search button is not displayed");
+
+		//Click the Search button (after the other fields are completed)
+		homePage.searchBtnClicked();
+
+		Thread.sleep(2000);
+
+		homePage.switchTo(0);
+
+		Assert.assertTrue(homePage.getUrl().contains("rooms"), "Rooms page is not displayed");	
+		homePage.closeDriver();
+
+		Thread.sleep(3000);
+	}
+
+	@Test 
+	public void verifyCheckIn() throws InterruptedException { 
+		Thread.sleep(3000);
+
+		//Validate that frame exists
+		homePage.frameWixHotels();
+		Thread.sleep(3000);
+
+		//Click the check in field
+		homePage.checkInClicked();
+		Thread.sleep(1000);
+
+		//switch to default
+		driver.switchTo().defaultContent();
+		Thread.sleep(1000);
+
+		//Click on a date (later than today)
+		//switch to check-in frame
+		homePage.switchToframe();
+		Thread.sleep(1000);
+
+		//select a date ->later than today
+		Thread.sleep(1000);
+		homePage.dataCheckInClicked();
+		Thread.sleep(3000);
+
+		homePage.frameWixHotels1();
+		Assert.assertEquals(homePage.actualDateCheckIn(), "30 Jun 2021");
+
+
+		Thread.sleep(1000);
+		WebDriverWait wait1 = new WebDriverWait(driver, 3000);
+		WebElement dataBtn = wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='check-in'][1]")));
+		dataBtn.click();
+
+		//switch to default
+		driver.switchTo().defaultContent();
+
+		//switch to check-in frame
+		homePage.switchToframe();
+
+		Thread.sleep(1000);
+
+		homePage.switchToframe();
+		homePage.nextMonthClicked();
+
+		Thread.sleep(1000);
+		homePage.dataCheckIn1Clicked();
+		Thread.sleep(3000);
+
+		homePage.frameWixHotels1();
+		Assert.assertEquals(homePage.actualDateCheckIn(), "30 Jul 2021");
+
+		//switch to default
+		driver.switchTo().defaultContent();
+
+		homePage.switchToframe();
+
+		Thread.sleep(1000);
+		homePage.dataCheckOutClicked();
+
+		Thread.sleep(1000);
+
+		homePage.frameWixHotels();
+		Assert.assertEquals(homePage.actualDateCheckOut(), "6 Aug 2021");
+
+		homePage.adultsUpClicked();
+		homePage.kidsUpClicked();
+
+		Thread.sleep(1000);
+		driver.switchTo().defaultContent();
+
+		Thread.sleep(1000);
+		homePage.frameWixHotels1();
+
+		Thread.sleep(1000);
+
+		//Validate that the Search button exists
+		Assert.assertTrue(homePage.searchBtnDisplayed(), "Search button is not displayed");
+
+		//Click the Search button (after the other fields are completed)
+		homePage.searchBtnClicked();
+
+		Thread.sleep(2000);
+
+		homePage.switchTo(0);
+
+		Assert.assertTrue(homePage.getUrl().contains("rooms"), "Rooms page is not displayed");	
+		homePage.closeDriver();
+
+		Thread.sleep(3000);
+	}
 }
